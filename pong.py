3017cb76cf082d0001dd42e3a3b5ccc4b367e99f
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-10-31 14:48:02 +0200

    set borders for the ball

--- pong.py
+++ pong.py
@@ -60,7 +60,28 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+ball.dx = 0.1
+ball.dy = -0.1
 
 # main game loop
 while True:
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
